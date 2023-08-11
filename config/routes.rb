7cc63c1d156eb0e0daa22aaa5c743ac903192a46
Vincent Ollivier <v@vinc.cc>
2023-08-12 01:02:56 +0200

    Add new emissions page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "/api/electricity/:country", to: "electricity#show"
   get "/api/electricity/:country/emissions", to: "electricity#emissions"
 
+  get "/electricity-emissions", to: "pages#electricity_emissions"
   root "pages#home"
 end
