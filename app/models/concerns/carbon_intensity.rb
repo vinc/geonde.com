4e7dbf6ddf7bafd23f66c275ba0971a32ab5d682
Vincent Ollivier <v@vinc.cc>
2023-08-12 01:02:15 +0200

    Update emission factors

--- app/models/concerns/carbon_intensity.rb
+++ app/models/concerns/carbon_intensity.rb
@@ -13,18 +13,30 @@ module CarbonIntensity
         oil: 0.777,
       }
 
-      # IPCC 2014
-      # UNECE 2020
+      # IPCC 2011 (http://www.ipcc-wg3.de/report/IPCC_SRREN_Annex_II.pdf)
       carbon = {
-        bio: 0.230,
-        gas: 0.430,
-        coal: 1.000,
-        oil: 0.777,
-        geo: 0.030,
-        hydro: 0.010,
-        nuclear: 0.005,
-        solar: 0.040,
+        bio: 0.018,
+        coal: 1.001,
+        gas: 0.469,
+        geo: 0.045,
+        hydro: 0.004,
+        nuclear: 0.016,
+        oil: 0.840,
+        solar: 0.046,
         wind: 0.012,
+      }
+
+      # NREL 2021 (https://www.nrel.gov/docs/fy21osti/80580.pdf)
+      carbon = {
+        bio: 0.052,
+        coal: 1.001,
+        gas: 0.486,
+        geo: 0.037,
+        hydro: 0.021,
+        nuclear: 0.013,
+        oil: 0.840,
+        solar: 0.043,
+        wind: 0.013,
       }
 
       1000 * carbon.map { |k, v| (data[k] || 0) * v }.sum / total
