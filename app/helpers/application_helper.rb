90a7cf75a3e872f63379071f210517ece1ea2c12
Vincent Ollivier <v@vinc.cc>
2023-08-16 13:10:02 +0200

    Refactor units in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,35 @@
 module ApplicationHelper
+  def time_ago(time)
+    time_tag(time, time_ago_in_words(time), title: time)
+  end
+
+  def duration_to_human(duration)
+    parts = []
+    if duration > 86400
+      parts << "#{(duration / 86400).to_i}h"
+      duration %= 86400
+    end
+    if duration > 3600
+      parts << "#{(duration / 3600).to_i}h"
+      duration %= 3600
+    end
+    if duration > 60
+      parts << "#{(duration / 60).to_i}m"
+      duration %= 60
+    end
+    if duration > 0
+      parts << "#{duration.to_i}s"
+    end
+    parts[0..1].join(" ")
+  end
+
+  def form_errors_heading(resource)
+    I18n.t(
+      "errors.messages.not_saved",
+      count: resource.errors.count,
+      resource: resource.class.model_name.human.downcase
+    )
+  end
   def link_to_source(name)
     case name
       when "elexon" then link_to("Elexon", "https://bmrs.elexon.co.uk/")
