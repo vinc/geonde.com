38277fb538151106059a228ce6622325120d4556
Vincent Ollivier <v@vinc.cc>
2023-08-18 15:08:45 +0200

    Fix geocoder result name

--- app/controllers/weather/conditions_controller.rb
+++ app/controllers/weather/conditions_controller.rb
@@ -10,7 +10,7 @@ class Weather::ConditionsController < ApplicationController
     raise ActiveRecord::RecordNotFound if res.nil?
 
     time = params[:time]&.to_time || Time.now
-    @city = res.city
+    @city = res.data["name"]
     @weather = Weather.new(latitude: res.latitude, longitude: res.longitude, time: time)
   end
 end
