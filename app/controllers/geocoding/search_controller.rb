8185d84fa493c7954b27340d92b5f373bd9b8f8d
Vincent Ollivier <v@vinc.cc>
2023-08-19 17:32:30 +0200

    Use rank in search

--- app/controllers/geocoding/search_controller.rb
+++ app/controllers/geocoding/search_controller.rb
@@ -3,6 +3,8 @@
 class Geocoding::SearchController < ApplicationController
   def index
     @query = params["q"]
-    @results = GeonamesData.where(name: @query).order(population: :desc)
+    @results = GeonamesData.search(@query).with_pg_search_rank
+    rank_max = @results.first&.pg_search_rank || 0
+    @results = @results.to_a.keep_if { |res| res.pg_search_rank == rank_max }
   end
 end
--- app/models/geonames_data.rb
+++ app/models/geonames_data.rb
@@ -1,4 +1,6 @@
 class GeonamesData < ApplicationRecord
   include PgSearch::Model
-  pg_search_scope :search, against: :name
+  pg_search_scope :search, against: :name, order_within_rank: "population DESC", using: {
+    tsearch: { normalization: 2 }
+  }
 end
