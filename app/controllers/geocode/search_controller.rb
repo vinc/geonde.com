d6524d8c53ffc6cd688f10c56c70c267b89436a1
Vincent Ollivier <v@vinc.cc>
2023-08-26 10:00:06 +0200

    Reorganize geocode views

--- app/controllers/geocode/search_controller.rb
+++ app/controllers/geocode/search_controller.rb
@@ -1,8 +1,11 @@
 class Geocode::SearchController < ApplicationController
   def index
+    @total = GeonamesData.count
     @query = params["q"]
-    @results = GeonamesData.search(@query).with_pg_search_rank
-    rank_max = @results.first&.pg_search_rank || 0
-    @results = @results.to_a.keep_if { |res| res.pg_search_rank == rank_max }
+    if @query.present?
+      @results = GeonamesData.search(@query).with_pg_search_rank
+      rank_max = @results.first&.pg_search_rank || 0
+      @results = @results.to_a.keep_if { |res| res.pg_search_rank == rank_max }
+    end
   end
 end
--- app/controllers/geocode_controller.rb
+++ app/controllers/geocode_controller.rb
@@ -1,5 +1,4 @@
 class GeocodeController < ApplicationController
   def index
-    @count = GeonamesData.count
   end
 end
