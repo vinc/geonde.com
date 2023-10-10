6f3c55421684e6dc336caba265f4d567211624f3
Vincent Ollivier <v@vinc.cc>
2023-10-10 22:31:58 +0200

    Add email uniqueness validation

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -2,8 +2,8 @@ class SubscribersController < ApplicationController
   def create
     @subscriber = Subscriber.new(email: params.require(:email))
     if @subscriber.save
-      flash[:notice] = "Thank you for subscribing"
     end
+    flash[:notice] = "Thank you for subscribing"
     redirect_to root_path
   end
 end
